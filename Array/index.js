d1cee64a4f50731d62f9f9f6e0f9e8b5fad7edc5
chumak8088@gmail.com <k.smulska@pro-capital.local>
2023-04-26 14:16:36 +0300

    Array

--- Array/index.js
+++ Array/index.js
@@ -5,10 +5,21 @@ for (let i = 0; i < length; i++) {
   arr.push(element);
 }
 
-arr.sort(function(a, b){return a-b}); 
+arr.sort(function(a, b) {
+  return a-b;
+}); 
 
 console.log("Відсортований масив: " + arr + "<br>");
-
 arr.splice(1, 3);
-
 console.log("Масив після видалення елементів з 2 по 4 (включно!): " + arr);
+
+if (arr.length >= 4) {
+  arr.slice (1, 3); 
+} else {
+  console.log("Масив занадто короткий для виконання splice().");
+}
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
